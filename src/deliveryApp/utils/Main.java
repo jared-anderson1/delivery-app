0f8248f97e100c7804b319324f22529c796a3c6c
VincentRegalado <Vincentregalado31@yahoo.com>
2025-11-23 19:54:44 -0800

    Implement user welcome and order process
    Added user creation and order placement in main method.

--- src/deliveryApp/utils/Main.java
+++ src/deliveryApp/utils/Main.java
@@ -7,6 +7,14 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        // Create a new User instance
+        User user = new User("test", "test");
 
+        System.out.println("Welcome, " + user.getName() + "!");
+
+        // Start the ordering process
+        user.placeOrder();
+
+        System.out.println("\nThank you for your order!");
     }
 }
