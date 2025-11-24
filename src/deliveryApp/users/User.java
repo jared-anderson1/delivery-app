79a1488dc1f89114c899488410dc88aac5420736
VincentRegalado <Vincentregalado31@yahoo.com>
2025-11-23 19:49:44 -0800

    Implement menu display and order placement methods
    Added methods for displaying menu and placing orders.

--- src/deliveryApp/users/User.java
+++ src/deliveryApp/users/User.java
@@ -1,5 +1,10 @@
 package deliveryApp.users;
 
+import deliveryApp.menu.MenuItem;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public abstract class User {
     protected String name;
     protected String password;
@@ -15,6 +20,46 @@ public abstract class User {
 
     public boolean login(String name, String password) {
         return this.name.equals(name) && this.password.equals(password);
+    }
+
+    public void displayMenu() {
+        MenuItem.displayMenu();
+    }
+
+    public void placeOrder() {
+        Scanner scanner = new Scanner(System.in);
+        List<MenuItem> order = new ArrayList<>();
+        double total = 0.0;
+        List<MenuItem> menuItems = MenuItem.getMenuItems();
+
+        while (true) {
+            displayMenu();
+            System.out.println("Enter the item number to order, or 0 to finish:");
+            int choice = scanner.nextInt();
+
+            if (choice == 0) {
+                break;
+            }
+
+            if (choice > 0 && choice <= menuItems.size()) {
+                MenuItem selectedItem = menuItems.get(choice - 1);
+                order.add(selectedItem);
+                total += selectedItem.getPrice();
+                System.out.println(selectedItem.getName() + " added to your order.");
+            } else {
+                System.out.println("Invalid item number.");
+            }
+        }
+
+        System.out.println("\nYour order:");
+        if (order.isEmpty()) {
+            System.out.println("No items in your order.");
+        } else {
+            for (MenuItem item : order) {
+                System.out.println("- " + item);
+            }
+            System.out.println("Total: $" + String.format("%.2f", total));
+        }
     }
 
     @Override
