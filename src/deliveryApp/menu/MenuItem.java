36a08d76cef2779ffad126463feafcf60f12a76b
VincentRegalado <Vincentregalado31@yahoo.com>
2025-11-23 19:48:06 -0800

    Add static menu items and display method

--- src/deliveryApp/menu/MenuItem.java
+++ src/deliveryApp/menu/MenuItem.java
@@ -1,8 +1,19 @@
 package deliveryApp.menu;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MenuItem {
     private String name;
     private double price;
+
+    private static final List<MenuItem> menuItems = new ArrayList<>();
+
+    static {
+        menuItems.add(new MenuItem("Pizza", 12.99));
+        menuItems.add(new MenuItem("Burger", 8.99));
+        menuItems.add(new MenuItem("Salad", 6.99));
+    }
 
     public MenuItem(String name, double price) {
         this.name = name;
@@ -17,9 +28,19 @@ public class MenuItem {
         return price;
     }
 
-    @Override
-    public String toString() {
-        return name + " -$" + price;
+    public static List<MenuItem> getMenuItems() {
+        return menuItems;
     }
 
+    public static void displayMenu() {
+        System.out.println("Menu:");
+        for (int i = 0; i < menuItems.size(); i++) {
+            System.out.println((i + 1) + ". " + menuItems.get(i));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name + " - $" + String.format("%.2f", price);
+    }
 }
